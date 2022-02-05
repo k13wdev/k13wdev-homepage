65f8c0a68e71a5bee716349cbd831f24079d4c02
KBogomolov <kirillbogomolow@yandex.ru>
2022-02-05 18:26:12 +0300

    upd

--- components/about.js
+++ components/about.js
@@ -5,20 +5,19 @@ const buttonClickHandler = () => {
 }
 
 export const About = () => {
+  const skills = ['HTML5', 'CSS3', 'SCSS', 'JavaScript', 'React', 'Next', 'Gulp', 'Webpack', 'Git', 'Figma', 'Linux']
+
   return (
     <section className='about'>
       <h2 className='title'>About</h2>
       <p className='text'>Hey! My name is Kirill and I love programming. I got interested in frontend development quite recently. Here are a few technologies I can work with:</p>
       <ul className='numbered-list'>
-        <li><span aria-hidden="true">00.</span>HTML5</li>
-        <li><span aria-hidden="true">01.</span>Pug</li>
-        <li><span aria-hidden="true">02.</span>CSS3</li>
-        <li><span aria-hidden="true">03.</span>SCSS</li>
-        <li><span aria-hidden="true">04.</span>JavaScript</li>
-        <li><span aria-hidden="true">05.</span>Gulp</li>
-        <li><span aria-hidden="true">06.</span>Webpack</li>
-        <li><span aria-hidden="true">07.</span>Git</li>
-        <li><span aria-hidden="true">08.</span>Figma</li>
+        {
+          skills
+            .map((skill, idx) => {
+              return <li key={idx}><span aria-hidden="true">{idx <= 9 ? '0' : ''}{idx}.</span>{skill}</li>
+            })
+        }
       </ul>
 
       <button className='about__button button' onClick={buttonClickHandler}>My portfolio</button>
